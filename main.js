65777326fb2c944796181c293b48cd95b648a42f
bodya17 <levantovychbogdan@gmail.com>
2016-06-13 13:26:57 +0300

    minor changes

--- main.js
+++ main.js
@@ -1,31 +1,22 @@
 function getJSON(selectors, context) {
 
 	context = context || document.body;
-	var result = [],
-		group = {},
-		firstTime = true,
-		index = 0;
+	var result = [], category;
 
 	for (var selector in selectors) {
 		if (selectors.hasOwnProperty(selector)) {
-		    var cateory = Array.prototype.slice.call(context.querySelectorAll(selectors[selector]));
-		    cateory.forEach(function(el, i, arr) {
-		    	arr[i] = el.textContent;
-
-		    	if (firstTime) {
-		    		group = {};
-	    			group[selector] = arr[i];
-		    		result.push(group);
-		    	}
-		    	else {
-		    		result[index++][selector] = arr[i];
-		    	}
-		    });
+		  category = context.querySelectorAll(selectors[selector]);
+			for (var i = 0, len = category.length; i < len; i++) {
+	    	if (result[i]) {
+					result[i][selector] = category[i].textContent;
+	    	}
+	    	else {
+					result.push({selector: category[i].textContent});
+	    	}
+	    }
 		}
-		firstTime = false;
-		index = 0;
 	}
-    return JSON.stringify(result, null, 2);
+  return JSON.stringify(result, null, 2);
 }
 
 getJSON({
@@ -34,4 +25,3 @@ getJSON({
 	type: '._pac',
 	status: '._42ft'
 }, document.getElementById('contentArea'));
-//blbalba

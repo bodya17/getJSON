09133f6cd6666a59e393a4b4a39c2b344bf48241
bodya17 <levantovychbogdan@gmail.com>
2016-06-13 13:39:50 +0300

    correct bug with name property

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 function getJSON(selectors, context) {
 
-	context = context || document.body;
-	var result = [], category;
+  context = context || document.body;
+  var category, // category for search (name, type etc.)
+			result = []; // array of categories
 
-	for (var selector in selectors) {
-		if (selectors.hasOwnProperty(selector)) {
-		  category = context.querySelectorAll(selectors[selector]);
-			for (var i = 0, len = category.length; i < len; i++) {
-	    	if (result[i]) {
-					result[i][selector] = category[i].textContent;
-	    	}
-	    	else {
-					result.push({selector: category[i].textContent});
-	    	}
-	    }
-		}
-	}
+  for (var selector in selectors) {
+    if (selectors.hasOwnProperty(selector)) {
+      category = context.querySelectorAll(selectors[selector]);
+      for (var i = 0, len = category.length; i < len; i++) {
+        if (result[i]) {
+          result[i][selector] = category[i].textContent;
+        }
+        else {
+					var obj = {};
+					obj[selector] = category[i].textContent;
+          result.push(obj);
+        }
+      }
+    }
+  }
   return JSON.stringify(result, null, 2);
 }
 
 getJSON({
-	name: '._gll a',
-	membersCount: '._glo > div:last-child > ._ajw:first-child > ._52eh',
-	type: '._pac',
-	status: '._42ft'
+  name: '._gll a',
+  membersCount: '._glo > div:last-child > ._ajw:first-child > ._52eh',
+  type: '._pac',
+  status: '._42ft'
 }, document.getElementById('contentArea'));
